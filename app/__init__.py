72e829f80687895ea95b38f79934f120da2d74e8
Jordon Marchesano <102629027+cHoMpDiSs@users.noreply.github.com>
2023-01-11 21:19:48 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.combining import OrTrigger
 from apscheduler.triggers.cron import CronTrigger
 
-trigger = OrTrigger([CronTrigger(hour=5),CronTrigger(hour=15),CronTrigger(hour=20)])
+trigger = OrTrigger([CronTrigger(hour='5'),CronTrigger(hour='15'),CronTrigger(hour='20')])
 
 sched = BackgroundScheduler()
 sched.add_job(flight_data.update_flight_data, trigger)
