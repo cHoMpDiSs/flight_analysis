43d0aabbdb1ea290e4355b7f6cc439fd31c595f9
cHoMpDiSs <jordonomarchesano@gmail.com>
2023-01-18 19:21:43 -0800

    move aps scheduler to __init__

--- run.py
+++ run.py
@@ -1,21 +1,5 @@
 from app import app, views
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.combining import OrTrigger
-from apscheduler.triggers.cron import CronTrigger
-
-trigger = CronTrigger(minute='*/1')
-
-
-from app.functions import plotting
-from app.flight_data import update_flight_data, cut_off_delay
-
-sched = BackgroundScheduler()
-sched.add_job(update_flight_data, trigger)
-sched.add_job(cut_off_delay, trigger)
-sched.add_job(plotting, trigger)
-
-sched.start()
 
 
 if __name__ == "__main__":
