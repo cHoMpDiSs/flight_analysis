753da36b225f5dd9324b78c51929e10b76783747
cHoMpDiSs <jordonomarchesano@gmail.com>
2023-01-18 18:17:13 -0800

    apscheduler 1

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.combining import OrTrigger
 from apscheduler.triggers.cron import CronTrigger
 
-trigger = CronTrigger(hour='*/4')
+trigger = CronTrigger(minute='*/1')
 
 
 from .functions import plotting
