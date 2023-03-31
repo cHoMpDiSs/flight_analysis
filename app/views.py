0b8ac1b919315dd5fcd3c60d856a5d71c35852ca
jordon <jordonomarchesano@gmail.com>
2023-03-31 11:57:14 -0700

    frontend_update_1

--- app/views.py
+++ app/views.py
@@ -6,7 +6,8 @@ from .functions import panda, db_query, plotting
 @app.route("/")
 def home():
     flight_delays_usa, flight_delays_ww, flight_cancellations_usa, flight_cancellations_ww = db_query()
-    return render_template('index.html',data1=flight_delays_usa, data2=flight_delays_ww, data3=flight_cancellations_usa, data4=flight_cancellations_ww)
+    return render_template('index.html',data1=flight_delays_usa, data2=flight_delays_ww, data3=flight_cancellations_usa,
+     data4=flight_cancellations_ww)
 
 @app.route("/pandas")
 def pandas():
